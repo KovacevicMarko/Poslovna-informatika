9b3db90dac7b28a11d6b1c7f012737226ff6720c
nikola <nikola.petrovic94@gmail.com>
2016-06-29 18:41:13 +0200

    dodata procedura koja se pokrece pri kliringu

---
+++ Modeli i skripta/KliringProcedura.sql
@@ -0,0 +1,21 @@
+if object_id ('KliringProcedura') is not null
+	drop procedure KliringProcedura; 
+go
+CREATE PROCEDURE KliringProcedura
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	update ANALITIKA_IZVODA
+	set ASI_STATUS = 'P'
+	where ASI_TIPGRESKE = 1 and ASI_STATUS = 'E';
+
+	-- Kad se dobije odgovor od narodne banke, treba update-ovati stanja
+	-- S obzirom da nemamo narodnu banku, update-ovanje nije realizovano
+	-- ali moze da se realizuje (videti kod u proceduri za uplatu)
+END
+GO
